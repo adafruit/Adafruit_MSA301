aa30a50b567b8f2c7912487da32b0a72d5d6f754
Doxygen CI <travis@travis-ci.org>
2020-05-26 02:56:36 +0000

    Deploy code docs to GitHub Pages Travis build:
    Commit:

--- html/search/all_0.js
+++ html/search/all_0.js
@@ -2,5 +2,6 @@ var searchData=
 [
   ['adafruit_5fmsa301',['Adafruit_MSA301',['../class_adafruit___m_s_a301.html',1,'Adafruit_MSA301'],['../class_adafruit___m_s_a301.html#a7a425bd5715ed7b325f2cd4ce025b63b',1,'Adafruit_MSA301::Adafruit_MSA301()']]],
   ['adafruit_5fmsa301_2ecpp',['Adafruit_MSA301.cpp',['../_adafruit___m_s_a301_8cpp.html',1,'']]],
-  ['adafruit_5fmsa301_2eh',['Adafruit_MSA301.h',['../_adafruit___m_s_a301_8h.html',1,'']]]
+  ['adafruit_5fmsa301_2eh',['Adafruit_MSA301.h',['../_adafruit___m_s_a301_8h.html',1,'']]],
+  ['adafruit_20msa301_20accelerometer_20breakout',['Adafruit MSA301 Accelerometer Breakout',['../index.html',1,'']]]
 ];
---
+++ html/search/pages_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['adafruit_20msa301_20accelerometer_20breakout',['Adafruit MSA301 Accelerometer Breakout',['../index.html',1,'']]]
+];
